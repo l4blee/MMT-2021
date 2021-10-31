a8fc7638e7732a0dae4351cefa763587eb8e6c69
l4blee <l4blee@yandex.ru>
2021-10-31 18:19:58 +0700

    upd

--- main.py
+++ main.py
@@ -16,11 +16,10 @@ pas_auto_prob = 847000 / total_cars
 total_probs = dict()
 keys = list(com_prob.keys()) + list(pas_prob.keys())
 for i in keys:
-    total_probs[str(i)] = (com_prob.get(i, 0) * com_auto_prob
-                           + pas_prob.get(i, 0) * pas_auto_prob) / 2
+    total_probs[str(i)] = (com_prob.get(i, 0) * com_auto_prob + pas_prob.get(i, 0) * pas_auto_prob)
 
 # print(total_probs)
-print(sum(total_probs.values()))
+# print(sum(total_probs.values()))
 '''df = pd.concat([pas, com])
 fig = core.get_plot(df, 3)
 
